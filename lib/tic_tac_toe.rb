8eb9aacc176f6fd7e9d74d50f653ba9d4ee37b17
Betul Kaya <betulkaya23@hotmail.com>
2020-03-07 18:10:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,38 +27,25 @@ def input_to_index(index)
 end
 
 def move(board,position,token)
-  
+
 end
 def position_taken?(board,index)
   if board[index]==""||board[index]==" "||board[index]== nil
     return false
-    
+
   else
     true
-    
+
   end
-  
+
 end
 
 def valid_move?(board,index)
   if position_taken(board,index)==false && index.between?(0,8)==true
     true
-    
+
   else
     false
-    
+
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
