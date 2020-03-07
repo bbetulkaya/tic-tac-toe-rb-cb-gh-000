978c7bf831d38ccf7cbbd8d30cef4337c1fb40ca
Betul Kaya <betulkaya23@hotmail.com>
2020-03-07 18:33:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -92,3 +92,38 @@ def draw?(board)
     return false
   end
 end
+
+def over?(board)
+  if won(board)==true || draw(board)==true || full(board)==true
+    return true
+  else
+    return false
+  end
+end
+
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
